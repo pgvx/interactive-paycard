f1b7b86564a29e1655fd6de167004f43769278d5
Sherif Elmetwally <sge5@njit.edu>
2019-11-01 16:19:04 -0400

    update cvv

--- flipCard.js
+++ flipCard.js
@@ -1,11 +1,13 @@
 const button = document.querySelector('button');
 const front = document.querySelector('.front');
 const back = document.querySelector('.back');
+const form = document.querySelector('.form');
 
 let cardIsFlipped = false;
 let hasError = false;
 
 const cvvInput = document.querySelector('.cvvInput');
+const cvvContainer = document.querySelector('.cvvContainer');
 
 const cardNumberInput = document.querySelector('.cardNumberInput');
 const monthSelect = document.querySelector('.monthSelect');
@@ -14,18 +16,15 @@ const cardHolderInputField = document.querySelector('.cardHolderInput');
 
 const frontSideInputs = [cardHolderInputField, cardNumberInput, yearSelect, monthSelect];
 
-frontSideInputs.forEach( (input) => {
-    input.addEventListener('click', () => {
+form.addEventListener('click' , (e) => {
+    if (e.target !== cvvInput) {
         front.style.display = "flex"
         back.style.display = "none";
-        cardIsFlipped = false;
-    })
+    }
 })
 cvvInput.addEventListener('click', () => {
-    console.log('hits');
     back.style.display = "flex";
     front.style.display = "none";
-    cardIsFlipped = true;
 });
 
 
@@ -45,7 +44,8 @@ const updateDisplayFunctions = {
                 hasError = false
                 hasError ? addError() : removeError();
             }
-        } else {
+        } 
+        else {
             document.querySelector('.cardNumber').innerHTML =  "number please...";
             hasError = true;
             document.querySelector('.errorMessage').style.display = "block";
@@ -60,27 +60,24 @@ const updateDisplayFunctions = {
         } else {
             document.querySelector('.name').innerHTML = input || "Your Name";
         }
-    } 
+    },
+    updateCVV: function updateCVV() {
+        cvvContainer.innerHTML = this.value;
+    }
 }
 
-
-const dateString = document.querySelector('.date').innerHTML;
-let monthValue = dateString.slice(0,2);
-console.log('monthValue: ', monthValue)
-
 monthSelect.addEventListener('change', function() {
-    console.log('this.value: ', this.value)
-    monthValue = this.value;
+    let monthValue = this.value;
     document.querySelector('.monthValue').innerHTML = monthValue;
   }, false);
 
 yearSelect.addEventListener('change', function() {
-    console.log('this.value: ', this.value)
-    yearValue = this.value;
+    let yearValue = this.value;
     document.querySelector('.yearValue').innerHTML = yearValue;
 }, false);
 
 
+// KEY PRESS FUNCTIONS
 cardNumberInput.onkeydown = updateDisplayFunctions.updateCardNumber;
 cardNumberInput.onkeyup = updateDisplayFunctions.updateCardNumber;
 cardNumberInput.onkeypress = updateDisplayFunctions.updateCardNumber;
@@ -88,6 +85,10 @@ cardNumberInput.onkeypress = updateDisplayFunctions.updateCardNumber;
 cardHolderInputField.onkeydown = updateDisplayFunctions.updateName;
 cardHolderInputField.onkeyup = updateDisplayFunctions.updateName;
 cardHolderInputField.onkeypress = updateDisplayFunctions.updateName;
+
+cvvInput.onkeydown = updateDisplayFunctions.updateCVV;
+cvvInput.onkeyup = updateDisplayFunctions.updateCVV;
+cvvInput.onkeypress = updateDisplayFunctions.updateCVV;
 
 const addError = () => {
     document.querySelector('.cardNumber').classList.add('error');
